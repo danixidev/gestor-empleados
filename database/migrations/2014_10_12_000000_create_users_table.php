7e79298edcf7d42327ceb7ccf290defed9b47c57
Dani <danixidev@gmail.com>
2021-12-01 14:28:40 +0100

    Created users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->double('salary');
+            $table->enum('role', ['directive', 'human-resources', 'employee']);
+            $table->String('api_token')->unique()->nullable();
             $table->timestamps();
         });
     }
